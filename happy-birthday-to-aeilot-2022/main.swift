81bb77aa4e38f13ec291220f15db381a2c05468b
Mr. Will <mr.will.com@outlook.com>
2022-03-13 12:13:42 +0800

    Update main.swift

--- happy-birthday-to-aeilot-2022/main.swift
+++ happy-birthday-to-aeilot-2022/main.swift
@@ -1,4 +1,10 @@
 import Foundation
 
-print("Hello, World!")
+let message = "SGFwcHkgQmlydGhkYXkgdG8gQGFlaWxvdCDwn46CIPCfjok="
+
+let data = NSData(base64Encoded: message, options: NSData.Base64DecodingOptions(rawValue: 0))
+
+let decoded = NSString(data: data! as Data, encoding: String.Encoding.utf8.rawValue)
+
+print(decoded!)
 
